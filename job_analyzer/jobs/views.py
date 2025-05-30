06335f3faf7d8f28295c4affed23ed0d00619ca1
mfurqaniftikhar <mfurqaniftikhar00@gmail.com>
2025-05-30 03:47:20 -0700

    Add files via upload

--- job_analyzer/jobs/views.py
+++ job_analyzer/jobs/views.py
@@ -1,8 +1,17 @@
-# jobs/views.py
-
+import json
 from django.shortcuts import render
-from .models import Job
 
 def dashboard(request):
-    jobs = Job.objects.all().order_by('-date_posted')[:200]
-    return render(request, "jobs/dashboard.html", {"jobs": jobs})
+    # Example data (replace with your querysets)
+    job_titles = {'Python Developer': 10, 'Data Scientist': 7, 'Frontend Dev': 5,'Backend':11}
+    skills = {'java':15,'Python': 12, 'Django': 9, 'React': 6}
+    cities = {'Lahore': 7, 'Karachi': 6, 'Islamabad': 4}
+    sources = {'Indeed': 9, 'Glassdoor': 8}
+
+    context = {
+        'job_titles': json.dumps(job_titles),
+        'skills': json.dumps(skills),
+        'cities': json.dumps(cities),
+        'sources': json.dumps(sources),
+    }
+    return render(request, "jobs/dashboard.html", context)
